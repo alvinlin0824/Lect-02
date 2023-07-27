01716dfcd2a93c55fe1719c99eae7f952baa428c
alvinlin0824 <alvin.lin@abbott.com>
2023-07-26 22:47:56 -0700

    Commit From HVS

--- 1008.cpp
+++ 1008.cpp
@@ -1,21 +1,26 @@
 #include<stdio.h>
 int main(){
     int a;
-    scanf(" %d",&a);
+    scanf("%d",&a);
     
     int sum = 0;
-    while (a > 10) {
+
+    if (a >= 10) {
+    while (a >= 10) {
       int remainder = a % 10;
       a = a / 10;
-      if (a > 10) {
+      if (a >= 10) {
         sum = sum + remainder;
-      } 
+        } 
       else {
         sum = sum + remainder + a;
+        }
       }
-    //   printf("Remainder: %d\n",remainder);
-    //   printf("Quotient: %d\n",a);
-    }
+    } 
+    else sum = a;
     printf("%d\n",sum);
     return 0;
 }
+
+    //   printf("Remainder: %d\n",remainder);
+    //   printf("Quotient: %d\n",a);
