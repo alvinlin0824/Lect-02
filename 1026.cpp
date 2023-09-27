a2b7ba9b3ea499eedcaa2a2572fe8c6e2aa7e4ec
alvinlin0824 <alvin.lin@abbott.com>
2023-09-27 14:21:44 -0700

    Commit From VS

---
+++ 1026.cpp
@@ -0,0 +1,17 @@
+// A Ming felt that his computer was often posted randomly by friends when it was left at school, 
+// so he wanted to write a password verification program to protect his privacy.
+// He didn't want to make his password too complex in case he forgot and couldn't log in, 
+// so the password only contained numbers.
+// He also doesn’t want to be like a cash machine, which locks the card 
+// after entering the wrong password more than three times, 
+// so he hopes that the program can try the password an unlimited number of times until the correct password is entered.
+// Can you help him design this program so that his friends can no longer steal his computer and post?
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+    return 0;
+}
