34cac8900e5bb5283938c08cf50af8ad42aac04a
alvinlin0824 <alvin.lin@abbott.com>
2023-09-27 21:03:32 -0700

    Commit From HVS

--- 1026.cpp
+++ 1026.cpp
@@ -11,7 +11,25 @@
 using namespace std;
 
 int main(){
+    //  long long int data type in C++ is used to store 64-bit integers. 
+    //  It is one of the largest data types to store integer values, 
+    //  unlike unsigned long long int both positive and negative
+    long long correct_password;
 
+    cin >> correct_password;
 
+    while (true)
+    {
+        long long attempt;
+        cin >> attempt;
+        if (correct_password != attempt){
+            cout << "Wrong Password!" << endl;
+        }
+        else{
+            cout << "Correct!" << endl;
+            break;
+        }
+    }
+    
     return 0;
 }
