90d95f3898d240adb370e8db66a0010aa3bfeaaf
alvinlin0824 <alvin.lin@abbott.com>
2023-10-30 22:59:52 -0700

    Commit From HVS

--- 1008.cpp
+++ 1008.cpp
@@ -1,29 +1,26 @@
 // n = 1234567
 // ans = 1+2+3+4+5+6+7 = 28 
 
-#include<stdio.h>
-int main(){
-    int a;
-    scanf("%d",&a);
-    
-    int sum = 0;
+#include<iostream>
+using namespace std;
 
-    if (a >= 10) {
-    while (a >= 10) {
-      int remainder = a % 10;
-      a = a / 10;
-      if (a >= 10) {
-        sum = sum + remainder;
-        } 
-      else {
-        sum = sum + remainder + a;
-        }
-      }
-    } 
-    else sum = a;
-    printf("%d\n",sum);
-    return 0;
+int main(){
+   
+  int n = 0;
+  int sum = 0;
+  cin >> n;
+
+  while (n > 0)
+  {
+    int remainder;
+    remainder = n % 10;
+    sum += remainder;
+    n /= 10;
+  }
+
+  cout << sum << endl ;
+  
+
+  return 0;
 }
 
-    //   printf("Remainder: %d\n",remainder);
-    //   printf("Quotient: %d\n",a);
