b04d47c2c5c7e1479b0426304168f9a8ffa035e2
alvinlin0824 <alvin.lin@abbott.com>
2023-11-01 13:20:22 -0700

    Commit From VS

--- 3022.cpp
+++ 3022.cpp
@@ -1,39 +1,37 @@
 // The existing sequence is as follows: 1,2,3,4,5,6,7,8,9,10
 // Please index as:
 
-//      sum of odd numbers
-//      sum of even numbers
-//      sum all its numbers
-
-// Input Format
-
-// string (a string of numbers)
-// Output Format
-
-// Integer (sum of odd numbers)
-// Integer (the sum of even digits)
-// Integer (sum of all numbers)
+// Sum of odd numbers
+// Sum of even numbers
+// Add up all its numbers
 
 #include<iostream>
-#include <iomanip> 
-#include <string>
+#include<string>
 using namespace std;
 
 int main(){
-    
+   
     string str;
-
+    int sum_odd = 0;
+    int sum_even = 0;
     cin >> str;
     
+    // for (int i = 0; i <= str.length() - 1; i+=2)
+    // {
     
-    for (int i = 0; i <= str.length(); i+=2)
-    {
-        cout << str[i] << endl;
-    }
-
-    cout << str.length();
-    cout << str[18];
-
+    //    if ((int(str[i]) - 48) % 2 == 0) {
+    //       sum_even += (int(str[i]) - 48);
+    //    } 
+    //    else {
+    //       sum_odd += (int(str[i]) - 48);
+    //    }
+    
+    // }
+    
+    cout << str.length() << endl;
+    // cout << sum_odd << endl << sum_even << endl << sum_odd + sum_even << endl;
     return 0;
 }
+
+
 
